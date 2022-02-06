f12c98b0301956a2e3c53136ffd4e65af3b6863a
mehmetfatihtuncer <68771760+mehmetfatihtuncer@users.noreply.github.com>
2022-02-06 16:59:43 +0300

    Update app.js
    you can see degree in console

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 let weather = {
-    "apikey" : "700e32c17064884435e55e54918aba1c"
-}
+    "apikey" : "700e32c17064884435e55e54918aba1c",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city +
+            "&units=metric&appid="
+            + this.apikey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {temp} = data.main;
+        console.log(temp)
+    }
+};
